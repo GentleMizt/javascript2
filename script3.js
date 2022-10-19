09846e3ab5e93b5b464d53d4be05c242b62dfda4
GentleMizt <olamideiyanda18@gmail.com>
2022-10-19 22:59:15 +0100

    resolved

--- script3.js
+++ script3.js
@@ -12,50 +12,72 @@ for (let i = 0; i < list.length; i++){
     console.log(updatedLists);
 }
 
+function checkUpdatedLists(isTrue){
+    for (let i = 0; i<updatedLists.length; i++){
+        if (input.value.toLowerCase() === updatedLists[i]){
+            isTrue = true;
+            input.value = '';
+            return isTrue;
+        }
+    }
+}
 
-button.addEventListener('click', () => {
-    
-    
-    if (input.value !== ""){
-        let found = false;
-        updatedLists.push(input.value);
-        console.log(updatedLists);
-        for (let p = 0; p < updatedLists.length; p++){
-            if (updatedLists[p] === input.value){
-                found = true;
-                updatedLists[p] = `${updatedLists[p]} x2`
-                input.value = '';
+    function addListItems(){
+        if (input.vale = ''){
+
+            if(checkUpdatedLists() === true){
+                alert('Item already exists');
+            }
+            else{
+                updatedLists.push.toLowerCase(input.value);
+                let newListItem = document.createElement('li');
+                
             }
         }
+    }
+    button.addEventListener('click', () => {
         
-        if (found){
-            // alert('Item already exists')
+        
+        if (input.value !== ""){
+            let found = false;
+            updatedLists.push(input.value);
+            console.log(updatedLists);
+            for (let p = 0; p < updatedLists.length; p++){
+                if (updatedLists[p] === input.value){
+                    found = true;
+                    updatedLists[p] = `${updatedLists[p]} x2`
+                    input.value = '';
+                }
+            }
+            
+            if (found){
+                // alert('Item already exists')
+            }
+            else{
+               
+                let listContent = document.createTextNode(input.value);
+
+                // body.append(ul);
+                ul.append(newListItem);
+                newListItem.append(listContent);
+                input.value = '';
+            } 
         }
-        else{
-            let newListItem = document.createElement('li');
-            let listContent = document.createTextNode(input.value);
+    });
 
-            // body.append(ul);
-            ul.append(newListItem);
-            newListItem.append(listContent);
+
+
+
+    updatedLists.forEach(updatedList => {  
+        if (updatedList === input.value){
+            found = true;
+            updatedList = `${updatedList} x2`
             input.value = '';
-        } 
-    }
-});
+        }
+    });   
 
 
-
-
-updatedLists.forEach(updatedList => {  
-    if (updatedList === input.value){
-        found = true;
-        updatedList = `${updatedList} x2`
-        input.value = '';
-    }
-});   
-
-
-// write a condition
-/* if the input value is already present in the list, return nothing
-if u want to take it a step further let the list value show a x2
-*/
+    // write a condition
+    /* if the input value is already present in the list, return nothing
+    if u want to take it a step further let the list value show a x2
+    */
